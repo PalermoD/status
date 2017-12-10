e626faba6f56331aa11b8399d51bd76ab61c2fb6
Palermo <palermo@bond.co>
2017-12-10 17:42:52 -0500

    update redux form

--- src/components/Auth/signin.js
+++ src/components/Auth/signin.js
@@ -1,25 +1,30 @@
 import React, {Component} from 'react'
-import {reduxForm} from 'redux-form'
+import {reduxForm, Field} from 'redux-form'
 
 
 class Signin extends Component {
+    constructor(props){
+        super(props)
+
+        this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    }
     handleFormSubmit({email, password}){
             console.log(email, password)
     }
 
     render(){
-        const {handleSubmit, fields: {email, password}} = this.props
+        const {handleSubmit} = this.props
 
         return (
             <div className="container">
-                <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <form onSubmit={handleSubmit(this.handleFormSubmit)}>
                     <fieldset className="form-group">
                         <label>Email: </label>
-                        <input {...email} className="form-control"/>
+                        <Field className="form-control" name="email" component="input" type="email" />
                     </fieldset>
                     <fieldset className="form-group">
                         <label>Password: </label>
-                        <input {...password} className="form-control"/>
+                        <Field className="form-control" name="password" component="input" type="password" />
                     </fieldset>
                     <button action="submit" className="btn btn-primary">Sign in</button>
                 </form>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,47 +2,37 @@ import React, {Component} from 'react'
 import {
     Navbar,
     Nav,
-    NavItem,
-    FormControl,
-    FormGroup,
-    Button
+    NavItem
 } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
 
 
 class Navigation extends Component {
-
-    componentDidMount(){}
-
-    componentWillMount(){}
-
     render(){
         return (
-
-                <Navbar inverse collapseOnSelect fixedTop>
-                    <Navbar.Header>
-                        <LinkContainer to="/">
-                            <Navbar.Brand>
-                                Status
-                            </Navbar.Brand>
+            <Navbar inverse collapseOnSelect fixedTop>
+                <Navbar.Header>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            Status
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <LinkContainer to="/categories">
+                            <NavItem eventKey={1}>Categories</NavItem>
                         </LinkContainer>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav pullRight>
-                            <LinkContainer to="/categories">
-                                <NavItem eventKey={1}>Categories</NavItem>
-                            </LinkContainer>
 
-                            <NavItem eventKey={1} href="#">Notifications</NavItem>
+                        <NavItem eventKey={1} href="#">Notifications</NavItem>
 
-                            <LinkContainer to="/signin">
-                                <NavItem eventKey={1} >Login</NavItem>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-
+                        <LinkContainer to="/signin">
+                            <NavItem eventKey={1} >Login</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
